6705fe431a88149f5e2ef7af73de2f380b65f372
ave <ave@DESKTOP-OQR929T>
2026-02-13 15:44:55 +0700

    second commmit

--- SDL/src/sdl/MyData.java
+++ SDL/src/sdl/MyData.java
@@ -15,6 +15,7 @@ public class MyData {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("INi program");
     }
     
 }
