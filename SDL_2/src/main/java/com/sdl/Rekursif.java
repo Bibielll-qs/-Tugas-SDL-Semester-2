a3353d1be2aede631e55535cf2ef4997474bcfb0
LENOVO <LENOVO@LAPTOP-I450PEPB>
2026-03-06 17:36:49 +0700

    commit 1

--- SDL_2/src/main/java/com/sdl/Rekursif.java
+++ SDL_2/src/main/java/com/sdl/Rekursif.java
@@ -10,93 +10,108 @@ package com.sdl;
  * @author puspa
  */
 public class Rekursif {
+
     /**
      * Fungsi rekursif faktorial
+     *
      * @param n
-     * @return 
+     * @return
      */
-    public static int faktorial(int n){
+    public static int faktorial(int n) {
         // base case
-        if (n ==0)
+        if (n == 0) {
             return 1;
-        else
-            //making progress
-            return n*faktorial(n-1);
+        } else //making progress
+        {
+            return n * faktorial(n - 1);
+        }
     }
-    
+
     /**
      * Fungsi rekursif deret fibonacci
+     *
      * @param suku
-     * @return 
+     * @return
      */
-    public static int fibo(int n){
+    public static int fibo(int n) {
         // base case
-        if ((n==1)|| (n==2))
-                return 1;
-        else
-            // making progress
-            return fibo(n-2)+fibo(n-1);
+        if ((n == 1) || (n == 2)) {
+            return 1;
+        } else // making progress
+        {
+            return fibo(n - 2) + fibo(n - 1);
+        }
     }
-    
+
     /**
      * Fungsi rekursif paijo
+     *
      * @param x
-     * @return 
+     * @return
      */
-    public static int paijo(int x){
-        return 0;
+    public static int paijo(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        return 2 * paijo(x - 1) + x * x;
     }
-    
+
     /**
      * Fungsi rekursif up and down
-     * @param n 
+     *
+     * @param n
      */
-    public static void upAndDown(int n){
-        
+    public static void upAndDown(int n) {
+
     }
-    
+
     /**
-     * Fungsi deret fibonaci dengan masukan suku ke-1, suku ke-2 dan 
+     * Fungsi deret fibonaci dengan masukan suku ke-1, suku ke-2 dan
+     *
      * @param suku1
      * @param suku2
      * @param jumlahSuku
-     * @return 
+     * @return
      */
-    public static int fibonaci(int suku1, int suku2, int sukuKe_N){
+    public static int fibonaci(int suku1, int suku2, int sukuKe_N) {
         return 0;
     }
 
     /**
-     * Fungsi untuk buat penjumlah larik jika diberi data larik dan posisi indeks yang akan dijumlahkan
+     * Fungsi untuk buat penjumlah larik jika diberi data larik dan posisi
+     * indeks yang akan dijumlahkan
+     *
      * @param data
      * @param i
-     * @return 
+     * @return
      */
     public static int arraySum(int[] data, int i) {
         return 0;
     }
-    
+
     /**
-     * Fungsi untuk mencari besar suku ke-N dari sebuah deret hitung
-     * Contoh: awal= 1, selisih = 3, indeks_suku = 5
-     * deretnya : [1,4,7,10,13], maka luarannya = 13
+     * Fungsi untuk mencari besar suku ke-N dari sebuah deret hitung Contoh:
+     * awal= 1, selisih = 3, indeks_suku = 5 deretnya : [1,4,7,10,13], maka
+     * luarannya = 13
+     *
      * @param awal
      * @param selisih
      * @param jumlah_suku
-     * @return 
+     * @return
      */
-    public static int deretHitung(int awal, int selisih, int indeks_suku){
+    public static int deretHitung(int awal, int selisih, int indeks_suku) {
         return 0;
     }
-    
+
     /**
      * Fungsi pembuat data larik deret hitung
+     *
      * @param awal
      * @param selish
      * @param indeks_suku
-     * @return 
+     * @return
      */
-    public static int[] getDeretHitung(int awal, int selish, int indeks_suku){
+    public static int[] getDeretHitung(int awal, int selish, int indeks_suku) {
         return null;
     }
 }
