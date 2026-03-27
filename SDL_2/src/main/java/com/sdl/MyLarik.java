1e591b67daf0e839b9a65c7ac0fd2868418fd035
LENOVO <LENOVO@LAPTOP-I450PEPB>
2026-03-27 14:40:06 +0700

    first commit

--- SDL_2/src/main/java/com/sdl/MyLarik.java
+++ SDL_2/src/main/java/com/sdl/MyLarik.java
@@ -240,5 +240,20 @@ public class MyLarik {
         }
         return -1;
     }
+    public int getChangeData(double[] data){
+        int n = data.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (data[j] > data[j+1]) {
+                    
+                    
+                }
+                
+            }
+        }
+    }
+    public int getMaxData(double[] data){
+        
+    }
 
 }
