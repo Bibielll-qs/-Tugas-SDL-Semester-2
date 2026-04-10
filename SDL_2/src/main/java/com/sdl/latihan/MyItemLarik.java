db340eda83501ddc71fca3c34d4ac0bed47a1ec4
LENOVO <LENOVO@LAPTOP-I450PEPB>
2026-04-10 13:49:58 +0700

    First commit

--- SDL_2/src/main/java/com/sdl/latihan/MyItemLarik.java
+++ SDL_2/src/main/java/com/sdl/latihan/MyItemLarik.java
@@ -54,15 +54,15 @@ public class MyItemLarik {
      * @param indeks
      */
     public void append(MyItem item, int indeks){
-        
+        this.data[indeks] = item;
     }
     
     /**
      * Fungsi untuk menghitung ukuran Array/Larik
      * @return 
      */
-    public int getDataSize(){
-        return 0;
+    public MyItem[] getDataSize(){
+        return data;
     }
     
     /**
@@ -71,9 +71,27 @@ public class MyItemLarik {
      * @return 
      */
     public int itemSearch(String item){
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(item)) {
+                return i;
+            }
+        }
         return -1;
     }
     
+    public void getChangeData(MyItem[] data, int indeks_a, int indeks_b){
+        String temp = data[indeks_a];
+        data[indeks_a] = data[indeks_b];
+        data[indeks_b] = temp;
+        return data;
+    }
+    
+    public void cetakData(MyItem[] ) {
+        for (int i = 0; i < data.length; i++) {
+            String d = data[i];
+            System.out.println(d);
+        }
+    }
     /**
      * Fungsi sorting berdasarkan masukan tipe.
      * tipe = 0, sorting berdasar String item secara ascending (kecil ke besar)
@@ -81,6 +99,18 @@ public class MyItemLarik {
      * @param type 
      */
     public void itemSort(int type){
-        
+        for (int i = 0; i < type-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < type; j++) {
+                if (data[j]< data[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                getChangeData(data, i, minIndex);
+            }
+            System.out.println("iterasi ke"+(i+1)+": ");
+            cetakData();
+        }
     }
 }
