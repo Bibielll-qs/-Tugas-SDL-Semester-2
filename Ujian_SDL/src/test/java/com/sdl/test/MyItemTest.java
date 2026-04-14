8f5133af27ebabd57f815e9202286d6dd5f35638
puspaningtyas <puspaningtyas@gmail.com>
2026-04-14 15:21:51 +0700

    Buat MyItemTest; test1() dan test2()

--- Ujian_SDL/src/test/java/com/sdl/test/MyItemTest.java
+++ Ujian_SDL/src/test/java/com/sdl/test/MyItemTest.java
@@ -23,4 +23,28 @@ public class MyItemTest {
     public void setUp() {
     }
     
+    @Test
+    public void test1(){
+        MyItem test = new MyItem();
+        test.setData("A");
+        test.setNumbers(4);
+        assertEquals("A",test.getData());
+        assertEquals(10,test.getNumbers());
+        
+        MyItem test1 = new MyItem("B",3);
+        assertEquals("B",test1.getData());
+        assertEquals(3,test1.getNumbers());
+        
+    }
+    
+    @Test
+    public void test2(){
+        MyItem test = new MyItem();
+        test.setData("A");
+        test.setNumbers(4);
+        assertEquals("A = 4",test);
+        
+        MyItem test1 = new MyItem("B",3);
+        assertEquals("B = 3",test1);
+    }
 }
