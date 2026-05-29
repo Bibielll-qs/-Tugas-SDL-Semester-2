7b6fcca8c54b86407f4aeeb5b2258d4fc8682387
puspaningtyas <puspaningtyas@gmail.com>
2026-05-29 11:16:46 +0700

    update skenario unit test

--- ujian/src/test/java/com/sdl/test/AntrianTugasTest.java
+++ ujian/src/test/java/com/sdl/test/AntrianTugasTest.java
@@ -5,6 +5,8 @@
  */
 package com.sdl.test;
 
+import com.sdl.ujian.AntrianTugas;
+import com.sdl.ujian.Tugas;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,6 +26,79 @@ public class AntrianTugasTest {
     
     @Test
     public void test1(){
+        AntrianTugas antrian = new AntrianTugas();
+        antrian.buatAntrianBaru();
+        int noAntrian = antrian.getNoAntrianTerakhir();
+        assertEquals(1,noAntrian);
+    }
+    
+    @Test
+    public void test2(){
+        AntrianTugas antrian = new AntrianTugas();
+        antrian.buatAntrianBaru();
+        antrian.buatAntrianBaru();
+        antrian.buatAntrianBaru();
         
+        int noAntrian = antrian.getNoAntrianTerakhir();
+        assertEquals(3,noAntrian);
+    }
+    
+    @Test
+    public void test3(){
+        AntrianTugas antrian = new AntrianTugas();
+        antrian.buatAntrianBaru();
+        antrian.buatAntrianBaru();
+        antrian.buatAntrianBaru();
+        
+        int noAntrian = antrian.getNoAntrianTerakhir();
+        assertEquals(3,noAntrian);
+        
+        antrian.updateStatusTugas(1,"T1",1);
+        TugasNode node = antrian.getFirst();
+        Tugas tugas = node.getData();
+        int status = tugas.getStatus();
+        assertEquals(1,status);
+    }
+    
+    @Test
+    public void test4(){
+        AntrianTugas antrian = new AntrianTugas();
+        antrian.buatAntrianBaru();
+        antrian.buatAntrianBaru();
+        antrian.buatAntrianBaru();
+        
+        int noAntrian = antrian.getNoAntrianTerakhir();
+        assertEquals(3,noAntrian);
+        
+        antrian.updateStatusTugas(1,"T1",2);
+        TugasNode node = antrian.getFirst();
+        Tugas tugas = node.getData();
+        int status = tugas.getStatus();
+        assertEquals(2,status);
+    }
+    
+    @Test
+    public void test5(){
+        AntrianTugas antrian = new AntrianTugas();
+        antrian.buatAntrianBaru();
+        antrian.buatAntrianBaru();
+        antrian.buatAntrianBaru();
+        
+        int noAntrian = antrian.getNoAntrianTerakhir();
+        assertEquals(3,noAntrian);
+        
+        antrian.updateStatusTugas(1,"T1",2);
+        TugasNode node = antrian.getFirst();
+        Tugas tugas = node.getData();
+        int status = node.getStatus();
+        assertEquals(2,status);
+        
+        AntrianTugas antrian1 = antrian.cariAntrian();
+        TugasNode node1 = antrian1.getFirst();
+        Tugas tugas1 = node1.getData();
+        int status = tugas1.getStatus();
+        int noAntrian = tugas1.getNoAntrian();
+        assertEquals(0,status);
+        assertEquals(2,noAntrian);
     }
 }
