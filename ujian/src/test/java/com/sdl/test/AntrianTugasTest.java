d3963066fed4ae1cca3e85fd3edbf331aadfda7e
LENOVO <LENOVO@LAPTOP-I450PEPB>
2026-05-29 17:34:37 +0700

    Final USIP Bila

--- ujian/src/test/java/com/sdl/test/AntrianTugasTest.java
+++ ujian/src/test/java/com/sdl/test/AntrianTugasTest.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.sdl.test;
+package com.sdl.test; //menyimpan file ke package com.sdl.test
 
-import com.sdl.ujian.AntrianTugas;
-import com.sdl.ujian.Tugas;
-import com.sdl.ujian.TugasNode;
-import org.junit.jupiter.api.BeforeEach;
+import com.sdl.ujian.AntrianTugas; //import isi dari class AntrianTugas
+import com.sdl.ujian.Tugas; //import isi dari class Tugas
+import com.sdl.ujian.TugasNode; //import isi dari class TugasNode
+import org.junit.jupiter.api.BeforeEach; //
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
