07a7f1609c58de87e549dcc3202a84ef7286c33b
puspaningtyas <puspaningtyas@gmail.com>
2026-05-29 10:24:33 +0700

    usip antrianTugas dengan linklist

---
+++ ujian/src/test/java/com/sdl/test/AntrianTugasTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sdl.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author puspa
+ */
+public class AntrianTugasTest {
+    
+    public AntrianTugasTest() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @Test
+    public void test1(){
+        
+    }
+}
