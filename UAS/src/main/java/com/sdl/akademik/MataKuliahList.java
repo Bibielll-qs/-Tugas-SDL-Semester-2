199081fc1c0fc0ff1886f19367a6927430a9ffc3
Bila <n.a.bilqissalsabila23@gmail.com>
2026-06-10 12:26:17 +0700

    UAS Bila Final

--- UAS/src/main/java/com/sdl/akademik/MataKuliahList.java
+++ UAS/src/main/java/com/sdl/akademik/MataKuliahList.java
@@ -8,17 +8,30 @@ import java.util.Comparator;
 public class MataKuliahList {
     private ArrayList<MataKuliah> data = new ArrayList();
 
+     /**
+      * constructor default
+      */
     public MataKuliahList() {
     }
     
-    
+    /**
+     * method masukkan data matkul
+     * @param mataKuliah 
+     */
     public void add(MataKuliah mataKuliah){
          data.add(mataKuliah);
     }
-    
+    /**
+     * fungsi menghitung jumlah data ukuran pada list
+     * @return 
+     */
     public int getSize(){
         return data.size();
     }
+    /**
+     * fungsi ambil data list MataKuliah
+     * @return 
+     */
     public ArrayList<MataKuliah> getData() { 
         return data; 
     }
